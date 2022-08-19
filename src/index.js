f70c70d238c1a1e924769850c5bdc6dd87f5058c
aliaksei.kagan <aliaksei.kagan@gmail.com>
2022-08-19 13:58:16 +0300

    Moved global styles to utils

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter } from 'react-router-dom';
 import App from './sections/App';
 import apollo from './services/apollo';
 import { ApolloProvider } from '@apollo/client';
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-  code {
-    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-      monospace;
-  }
-`;
+import { GlobalStyle } from 'utils/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
